5134122b1a9d31637b23279ca55ff52a9c2507e8
Lisa Lee <snickers495@gmail.com>
2018-01-14 18:58:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+ function justInvoke(fn){
+   return fn();
+ }
+ function setThisWithCall(fn, thisValue, arg){
+   return fn.call(thisValue, arg);
+ }
+ function setThisWithApply(fn, thisValue, arg){
+   return fn.apply(thisValue, arg);
+ }
+ function returnNewFunctionOf(fn, thisValue){
+   return fn.bind(thisValue);
+ }
