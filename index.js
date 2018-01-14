c4f149fe5ba95bdc0c6495ecdce09f0fece0a83c
Lisa Lee <snickers495@gmail.com>
2018-01-14 18:57:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
    return fn();
  }
  function setThisWithCall(fn, thisValue, arg){
-   return fn.call(thisValue, arg)
+   return fn.call(thisValue, arg);
  }
  function setThisWithApply(fn, thisValue, arg){
    return fn.apply(thisValue, arg);
