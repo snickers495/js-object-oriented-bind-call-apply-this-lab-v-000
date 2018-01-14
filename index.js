aea19e72a796bc4af8f7ae0ea0251fb496a85643
Lisa Lee <snickers495@gmail.com>
2018-01-14 18:56:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
  function justInvoke(fn){
    return fn();
  }
- function setThisWithCall(fn, name, age){
-   let callBack = fn(age)
+ function setThisWithCall(fn, name, arg){
    return callBack.call(name)
  }
